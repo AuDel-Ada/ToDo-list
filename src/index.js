708143a1ab7cdf3057a3f83b6c44aea1c97624a4
Aurelie D <adelaunay@palo-it.com>
2022-05-20 22:28:27 +0200

    Add bugsnag on index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+Bugsnag.start({
+  apiKey: '47b364838a91676125a82ec2b60ecfaa',
+  plugins: [new BugsnagPluginReact()]
+})
+
+const ErrorBoundary = Bugsnag.getPlugin('react')
+  .createErrorBoundary(React)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </ErrorBoundary>
+  , document.getElementById('root')
+)
+
+Bugsnag.notify(new Error('Test error'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
