7fee4be21231056eb3df95db361f38a6de8b2110
Aurelie D <adelaunay@palo-it.com>
2022-05-23 13:22:15 +0200

    Change to test Sentry APM

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Bugsnag from '@bugsnag/js'
-import BugsnagPluginReact from '@bugsnag/plugin-react'
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import App from "./App";
+
+Sentry.init({
+  dsn: "https://7322827349ba46a094353207b976886c@o1242693.ingest.sentry.io/6431844",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -13,22 +23,6 @@ import BugsnagPluginReact from '@bugsnag/plugin-react'
 //   document.getElementById('root')
 // );
 
-Bugsnag.start({
-  apiKey: '47b364838a91676125a82ec2b60ecfaa',
-  plugins: [new BugsnagPluginReact()]
-})
-
-const ErrorBoundary = Bugsnag.getPlugin('react')
-  .createErrorBoundary(React)
-
-ReactDOM.render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>
-  , document.getElementById('root')
-)
-
-Bugsnag.notify(new Error('Test error'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
