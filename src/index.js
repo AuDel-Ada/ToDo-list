498ef656b2382314c56f97a4ba25f44a9b75d9e1
Aurelie D <adelaunay@palo-it.com>
2022-05-23 13:25:21 +0200

    Add import reportWebVitals

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
+import reportWebVitals from './reportWebVitals';
 
 Sentry.init({
   dsn: "https://7322827349ba46a094353207b976886c@o1242693.ingest.sentry.io/6431844",
