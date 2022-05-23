92ccd04816bbcf2904ef0a5877577bce80b279b8
Aurelie D <adelaunay@palo-it.com>
2022-05-23 17:37:33 +0200

    Monitoring perf on index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
+import App from "./App";
+
+import { createBrowserHistory } from 'history';
+
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
-import App from "./App";
+
 import reportWebVitals from './reportWebVitals';
+
+
+const history = createBrowserHistory();
+
 
 Sentry.init({
   dsn: "https://7322827349ba46a094353207b976886c@o1242693.ingest.sentry.io/6431844",
-  integrations: [new BrowserTracing()],
+  integrations: [
+    // BrowserTracing integration to add automatic instrumentation for monitoring
+    // the performance of browser applications.
+    new BrowserTracing({
+      tracingOrigins: ["localhost", "https://damp-savannah-47038.herokuapp.com/", /^\//],
+      routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+    }),
+  ],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
